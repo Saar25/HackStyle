bdcd33c905190055c4cb12d6ebc8fbc5d8c376e3
MrTarnegol <37410028+Saar25@users.noreply.github.com>
2019-05-23 19:45:12 +0300

    Update HaxballTerror.js

--- HaxballTerror.js
+++ HaxballTerror.js
@@ -11,8 +11,8 @@ style.send = function() {
 
 function copyText(text) {
     var textArea = document.createElement("textarea");
-    textArea.value = text;
     document.body.appendChild(textArea);
+    textArea.value = text;
     textArea.focus();
     textArea.select();
     document.execCommand('copy');
@@ -48,28 +48,26 @@ function tagAll() {
     style.send();
 }
 
-var waiting = 5000;
-function addTagButton() {
+function addButtons() {
     var input = gameDocument.getElementsByClassName("input")[0];
-    if (input !== undefined) {
-        if (input.children[2] === undefined) {
-            var tag = input.children[1].cloneNode(false);
-            tag.style.marginLeft = "5px";
-            tag.innerText = "TagAll";
-            tag.onclick = tagAll;
-            input.appendChild(tag);
-            var print = input.children[1].cloneNode(false);
-            print.style.marginLeft = "5px";
-            print.innerText = "PrintAll";
-            print.onclick = printAll;
-            input.appendChild(print);
-        }
-        setTimeout(addTagButton, waiting);
-        if (waiting < 60000) waiting += 1000;
-    } else {
-        setTimeout(addTagButton, 3000);
-        waiting = 5000;
+    if (input === undefined) {
+        return;
     }
+    var tag = input.children[1].cloneNode(false);
+    tag.style.marginLeft = "5px";
+    tag.innerText = "TagAll";
+    tag.onclick = tagAll;
+    input.appendChild(tag);
+    var print = input.children[1].cloneNode(false);
+    print.style.marginLeft = "5px";
+    print.innerText = "PrintAll";
+    print.onclick = printAll;
+    input.appendChild(print);
 }
 
-addTagButton();
+setTimeout(function() {
+    var observer = new MutationObserver(addButtons);
+    var holder = gameDocument.body.getElementsByTagName("div")[0];
+    observer.observe(holder, {childList: true});
+    addButtons();
+}, 3000);
