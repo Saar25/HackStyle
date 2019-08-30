b381093f6ca9a48e4a75554cdad2ed7ca3c9c2a5
MrTarnegol <37410028+Saar25@users.noreply.github.com>
2019-08-31 01:45:06 +0300

    Update and rename HaxballTerror.js to HackStyle.js

--- HackStyle.js
+++ HackStyle.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Haxball Terror
+// @name         HackStyle.js
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Terror every room in Haxball
