32342be3b1761125dc612a2cc3e89edf07fd3a79
Saar <notnow@maybelater.com>
2020-03-20 14:10:41 +0200

    Create Scripts file Model

---
+++ src/main/java/hackstyle/scripts/parsing/HackStyleFile.java
@@ -0,0 +1,68 @@
+package hackstyle.scripts.parsing;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name = "hackstyle")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class HackStyleFile {
+
+    @XmlElementWrapper(name = "values")
+    @XmlElement(name = "value")
+    public List<Value> values;
+
+    @XmlElementWrapper(name = "scripts")
+    @XmlElement(name = "script")
+    public List<Script> scripts;
+
+    @Override
+    public String toString() {
+        return "HackStyleFile{" +
+                "\n\tvalues=" + values +
+                ", \n\tscripts=" + scripts +
+                "\n}";
+    }
+
+    @XmlRootElement(name = "value")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    private static class Value {
+
+        @XmlElement(name = "name")
+        private String name;
+
+        @XmlElement(name = "content")
+        private String content;
+
+        @Override
+        public String toString() {
+            return "Value{" +
+                    "name='" + name + '\'' +
+                    ", content='" + content + '\'' +
+                    '}';
+        }
+    }
+
+    @XmlRootElement(name = "script")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    private static class Script {
+
+        @XmlElement(name = "name")
+        private String name;
+
+        @XmlElement(name = "indicator")
+        private String indicator;
+
+        @XmlElement(name = "code")
+        private String code;
+
+        @Override
+        public String toString() {
+            return "Script{" +
+                    "name='" + name + '\'' +
+                    ", indicator='" + indicator + '\'' +
+                    ", code='" + code + '\'' +
+                    '}';
+        }
+    }
+
+}
