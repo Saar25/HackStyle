c9c565810e150df6a33f2ee558913fd5b3b6226c
Tarnegol <37410028+Saar25@users.noreply.github.com>
2020-03-27 13:41:32 +0300

    Update HackStyle.js

--- HackStyle.js
+++ HackStyle.js
@@ -8,10 +8,10 @@
 // @grant        none
 // ==/UserScript==
 
-var style = window.style = {};
+const style = window.style = {};
 
 style.nickname = "Style";
-
+style.nicknames = ["Style", "style", "סטייל"];
 style.avatars = "המהפכה".split("")
 style.destroy = "ఌ佹砕Ĉ⼈渻䬭॑⬺爂᰾ᜪ䉧ᬯ㍸᜖ᘬ慩㙲Ԑ瀩ञࠣ笋℉⨄㐷紪権ਛൽ㔙怅㬆癕⨴琐ḉ≼崪ᙻḻ⨂㈿爇䕦ᴵ灾㸏眮ቡᄸ܍礂海學㰌⬦ ѫ⼕ย⌑ḷ㠕ፃ㤕㌋␢眢砬ⵯ煾Ⱘ縂㉰匊㤀ह稫ᝈⴾㄫ㤒䉊؈ି㔞帱ॹࠚⰯἯ浸ἱᔞ፭ਮ娮ف㤮〕㱳⬗ᅷд甇ᜯ⩿ᤶⱁ戃 椏缁⼐獾⌱മ㈐ᵲ桨ԷᴑĿᑆ㨤ఴ⑸甈⨘Ѳ∯㘴䁔ح฾،ฌ"
 style.defaultAvatar = ":}";
@@ -49,7 +49,10 @@ class Timer {
     }
 }
 
-var gameDocument = window.gameDocument = document.getElementsByClassName('gameframe')[0].contentWindow.document
+var gameDocument;
+const initGameDocument = () => {
+    gameDocument = window.gameDocument = document.getElementsByClassName('gameframe')[0].contentWindow.document;
+}
 
 style.avatarTimer = new Timer(() => {
     if (style.chatBox() !== gameDocument.activeElement) {
@@ -58,76 +61,76 @@ style.avatarTimer = new Timer(() => {
     }
 }, 100);
 
-style.chatBox = function() {
+style.chatBox = () => {
     return gameDocument.getElementsByTagName("input")[0];
 }
 
-style.sendButton = function() {
+style.sendButton = () => {
     return gameDocument.getElementsByClassName("input")[0].getElementsByTagName("button")[0];
 }
 
-style.chatHistory = function() {
+style.chatHistory = () => {
     return gameDocument.getElementsByClassName("log ps")[0];
 }
 
-style.write = function(text) {
+style.write = (text) => {
     style.chatBox().value = text;
 }
 
-style.writeAppend = function(text) {
+style.writeAppend = (text) => {
     style.chatBox().value += text;
 }
 
-style.send = function() {
+style.send = () => {
     style.sendButton().click();
 }
 
-style.getMessage = function() {
+style.getMessage = () => {
     return style.chatBox().value;
 }
 
-style.setAvatar = function(avatar) {
+style.setAvatar = (avatar) => {
 	style.write("/avatar " + avatar);
 	style.send();
 }
 
-style.setNextAvatarIndex = function() {
+style.setNextAvatarIndex = () => {
     style.setAvatar(style.avatars[style.avatarIndex]);
     style.avatarIndex = (style.avatarIndex + 1) % style.avatars.length;
 }
 
-style.setDefaultIndex = function() {
+style.setDefaultIndex = () => {
     style.setAvatar(style.defaultAvatar);
     style.avatarIndex = 0;
 }
 
-style.startAvatarChanger = function() {
+style.startAvatarChanger = () => {
     style.avatarTimer.start();
 }
 
-style.stopAvatarChanger = function() {
+style.stopAvatarChanger = () => {
     style.avatarTimer.stop();
     style.setAvatar(style.defaultAvatar);
     style.avatarIndex = 0;
 }
 
-style.notice = function(message) {
-    let element = document.createElement("p");
+style.notice = (message) => {
+    const element = document.createElement("p");
     element.className = "notice";
     element.innerText = message;
     style.chatHistory().appendChild(element);
 }
 
-function removeLastAvatarSetMessage() {
-    var chat = style.chatHistory();
+const removeLastAvatarSetMessage = () => {
+    const chat = style.chatHistory();
     if (chat.lastChild.innerText == "Avatar set") {
         chat.lastChild.remove();
     }
 }
 
-function getAll() {
-    var players = gameDocument.querySelectorAll('[data-hook=name]');
-    var string = ""
+const getAll = () => {
+    const players = gameDocument.querySelectorAll('[data-hook=name]');
+    let string = ""
     for (var i = 0; i < players.length; i++) {
         var player = players[i].innerHTML.replace(/ /g, "_");
         string += "@" + player + " ";
@@ -135,9 +138,24 @@ function getAll() {
     return string;
 }
 
+const copyLastMessage = () => {
+    const children = style.chatHistory().children
+    const text = children[children.length - 1].innerText
+    const sender = text.slice(0, text.indexOf(": "))
+    var message = text.slice(text.indexOf(": ") + 2)
+    if (sender != style.nickname && text.indexOf(": ") != -1) {
+        for (let nickname of style.nicknames) {
+            const regexp = new RegExp(nickname, 'g');
+            message = message.replace(regexp, sender)
+        }
+        style.write(message)
+        style.send()
+    }
+}
+
+const messagesObserver = new MutationObserver(copyLastMessage);
 var jinxOn = false;
-var messagesObserver = new MutationObserver(copyLastMessage);
-function toggleJinx() {
+const toggleJinx = () => {
     jinxOn = !jinxOn
     if (jinxOn) {
         messagesObserver.observe(style.chatHistory(), {childList: true});
@@ -148,18 +166,7 @@ function toggleJinx() {
     }
 }
 
-function copyLastMessage() {
-    var children = style.chatHistory().children
-    var text = children[children.length - 1].innerText
-    var message = text.slice(text.indexOf(": ") + 2)
-    var sender = text.slice(0, text.indexOf(": "))
-    if (sender != style.nickname && text.indexOf(": ") != -1) {
-        style.write(message)
-        style.send()
-    }
-}
-
-function toggleAvatar() {
+const toggleAvatar = () => {
     if (style.avatarTimer.isRunning()) {
         style.stopAvatarChanger();
         removeLastAvatarSetMessage();
@@ -172,58 +179,58 @@ function toggleAvatar() {
     }
 }
 
-function spamMessage() {
+const spamMessage = () => {
     var message = style.getMessage();
     style.write(message);
     style.send();
     style.write(message);
 }
 
-function tagAll() {
-    var tag = getAll();
+const tagAll = () => {
+    const tag = getAll();
     if (tag) {
-        var message = style.getMessage();
-        var string = tag + message;
+        const message = style.getMessage();
+        const string = tag + message;
         style.write(string);
         style.send();
         style.write(message);
     }
 }
 
-function controlledSend() {
+const controlledSend = () => {
     style.send()
 }
 
-function addInGameButtons() {
-    var input = gameDocument.getElementsByClassName("input")[0];
+const addInGameButtons = () => {
+    const input = gameDocument.getElementsByClassName("input")[0];
     if (input !== undefined) {
         // controlled send
-        var send = input.children[1].cloneNode(false);
+        const send = input.children[1].cloneNode(false);
         send.style.marginLeft = "5px";
         send.innerText = "Send";
         send.onclick = controlledSend;
         input.appendChild(send);
         // tag
-        var tag = input.children[1].cloneNode(false);
+        const tag = input.children[1].cloneNode(false);
         tag.style.marginLeft = "5px";
         tag.innerText = "TagAll";
         tag.onclick = tagAll;
         input.appendChild(tag);
         // spam
-        var spam = input.children[1].cloneNode(false);
+        const spam = input.children[1].cloneNode(false);
         spam.style.marginLeft = "5px";
         spam.innerText = "Spam";
         spam.onclick = spamMessage;
         input.appendChild(spam);
         // avatar
-        var x = gameDocument.getElementsByClassName("bottom-section")[0].getElementsByClassName("buttons")[0].children[1];
-        var avatar = x.cloneNode(false);
+        const x = gameDocument.getElementsByClassName("bottom-section")[0].getElementsByClassName("buttons")[0].children[1];
+        const avatar = x.cloneNode(false);
         avatar.innerText = "Avatar: Off";
         avatar.setAttribute("data-hook", "avatarChanger");
         avatar.onclick = () => {avatar.innerText = toggleAvatar()};
         x.parentElement.appendChild(avatar);
         // copyMessages
-        var jinx = x.cloneNode(false);
+        const jinx = x.cloneNode(false);
         jinx.innerText = "Jinx: Off";
         jinx.setAttribute("data-hook", "jinx");
         jinx.onclick = () => {jinx.innerText = toggleJinx()};
@@ -233,18 +240,26 @@ function addInGameButtons() {
     }
 }
 
-function addButtons() {
+const addButtons = () => {
     addInGameButtons();
 }
 
-function onFrameChange() {
+const onFrameChange = () => {
     addButtons();
     style.avatarTimer.stop();
 }
 
-setTimeout(function() {
-    var observer = new MutationObserver(onFrameChange);
-    var holder = gameDocument.body.getElementsByTagName("div")[0];
-    observer.observe(holder, {childList: true});
-    addButtons();
-}, 3000);
+const start = () => {
+    const holder = gameDocument.getElementsByTagName("body")[0].getElementsByTagName("div")[0];
+    if (holder != null) {
+        const observer = new MutationObserver(onFrameChange);
+        observer.observe(holder, {childList: true});
+        addButtons();
+    } else {
+        initGameDocument();
+        setTimeout(start, 1000);
+    }
+}
+
+initGameDocument();
+setTimeout(start, 3000);
